c428720eff6a23c63ee06741cfbd7467b2cb4108
Benjamin VANDENBUSSCHE <13152@ecam.be>
2016-10-14 12:25:22 +0200

    minor change in AppResDet

--- AppResDet.php
+++ AppResDet.php
@@ -7,15 +7,21 @@
 	<body>
 		<b><h1>DETAIL DES RESERVATIONS</h1></b><br /><br />
 		<form method="post" name="ResDet">
-			<?php
-				Nom
-				<input type="text" />
-				Age
-				<input type="text" />
+			<table>
+				<?php
+					echo <tr>
+						echo <th>Nom</th>
+						echo <th><input type="text" /></th>
+					echo </tr>
+					echo <tr>
+						echo <th>Age</th>
+						echo <th><input type="text" /></th>
+					echo</tr>
+				?>
 				<input type="submit" value="Etape suivante" />
 				<input type="button" value="Retour à la page précédente" />
 				<input type="button" value="Annuler la réservation" />
-			?>
+				</table>
 		</form>
 	</body>
 </html>
