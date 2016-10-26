b80170286061a5a13141c2d7ee11eaec5b3a5a79
Benjamin Vandenbussche <Benjamin Vandenbussche>
2016-10-26 08:52:11 +0200

    Modified AppResVal

--- AppResVal.php
+++ AppResVal.php
@@ -11,6 +11,28 @@
 		<table>
 			<tr>
 				<th>Destination</th>
+				<th></th> 
+			</tr>
+			<tr>
+				<th>Nombre de places</th>
+				<th></th>
+			</tr>
+			<?php
+			session_start(); 
+			$count= $_SESSION["seats"];
+			while ($count > 0){
+				echo '<tr>
+					<th>Nom</th>
+					<th></th>
+				</tr>
+				<tr>
+					<th>Age</th>
+					<th></th>
+				</tr>';
+				$count--;
+			}
+			<tr>
+				<th>Assurance annulation</th>
 				<th></th>
 			</tr>
 		</table>
